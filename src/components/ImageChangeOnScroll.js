3ce20fd17608253477b9b2a412481621ae95939c
Michelle <rotariumihaela.bv@gmail.com>
2023-09-28 23:42:09 +0300

    updated styles

--- src/components/ImageChangeOnScroll.js
+++ src/components/ImageChangeOnScroll.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ImageToggleOnScroll from "./ImageToggleOnScroll";
 const ImageChangeOnScroll = () => {
-  var count =0;
+  const [mouseEventCnt, setMouseEventCnt] = useState(0);
+  var count = 0;
+
   return (
     <div>
-       <ImageToggleOnScroll
-        primaryImg="/black/aleksandra-sapozhnikova-oKpf7wGJzP0-unsplash.jpg"
-        secondaryImg="aleksandra-sapozhnikova-oKpf7wGJzP0-unsplash.jpg"
-        alt=""
-      />
-      &nbsp;&nbsp;&nbsp;
-      <ImageToggleOnScroll
-        primaryImg="/black/elijah-hail-PpjNNCWh2ng-unsplash.jpg"
-        secondaryImg="elijah-hail-PpjNNCWh2ng-unsplash.jpg"
-        alt=""  
-      />
+      <span>mouseEventCnt: ${mouseEventCnt}</span>
+      <div
+        onMouseOver={() => {
+          console.log(`Count : ${count++}`);
+          setMouseEventCnt(mouseEventCnt + 1);
+        }}
+      >
+        <ImageToggleOnScroll
+          primaryImg="/black/aleksandra-sapozhnikova-oKpf7wGJzP0-unsplash.jpg"
+          secondaryImg="aleksandra-sapozhnikova-oKpf7wGJzP0-unsplash.jpg"
+          alt=""
+        />
+        &nbsp;&nbsp;&nbsp;
+        <ImageToggleOnScroll
+          primaryImg="/black/elijah-hail-PpjNNCWh2ng-unsplash.jpg"
+          secondaryImg="elijah-hail-PpjNNCWh2ng-unsplash.jpg"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
